29e7c7d4287a421f5d131ccd11ba5aee8a0dee3c
Mead955568 <955568@swansea.ac.uk>
2023-09-05 23:11:26 +0100

    Added Line height variation script

--- Assets/Script/SetNavTargeting.cs
+++ Assets/Script/SetNavTargeting.cs
@@ -2,6 +2,8 @@ using TMPro;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class SetNavTargeting : MonoBehaviour
 {
@@ -9,6 +11,8 @@ public class SetNavTargeting : MonoBehaviour
     private TMP_Dropdown _navTargetDropDown;
     [SerializeField]
     private List<Target> _navTargetObjects = new List<Target>();
+    [SerializeField]
+    private Slider _navYOffset;
 
     private NavMeshPath _path; // Current Calculated Path
     private LineRenderer _lineRenderer; // Linerenderer To Display Path
@@ -29,7 +33,8 @@ public class SetNavTargeting : MonoBehaviour
         {
             NavMesh.CalculatePath(transform.position, _targetPosition, NavMesh.AllAreas, _path);
             _lineRenderer.positionCount = _path.corners.Length;
-            _lineRenderer.SetPositions(_path.corners);
+            Vector3[] calculatedPathAndOffset = AddLineOffset();
+            _lineRenderer.SetPositions(calculatedPathAndOffset);
         }
     }
     public void SetCurrentNavTarget(int selectedValue)
@@ -48,4 +53,19 @@ public class SetNavTargeting : MonoBehaviour
         _lineRenderer.enabled = _lineToggle;
         Debug.Log("Toggle Line Vis");
     }
+    public Vector3[] AddLineOffset() 
+    {
+        if (_navYOffset.value == 0)
+        {
+            return _path.corners;
+        }
+
+        Vector3[] calculatedLine = new Vector3[_path.corners.Length];
+        for (int i = 0; i < _path.corners.Length; i++)
+        {
+            calculatedLine[i] = _path.corners[i] + new Vector3(0, _navYOffset.value, 0);
+        }
+        return calculatedLine;
+    }
+
 }
